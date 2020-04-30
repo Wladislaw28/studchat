8c50d1a78ac5885399d0034df341d3dd7111705c
Vladislav Mihasev <vladislav.mihasev@engagesq.com>
2020-05-01 01:09:04 +0300

    remove gitignore files

--- node_modules/react-styleguidist/lib/client/utils/__tests__/getAst.spec.js
+++
@@ -1,7 +0,0 @@
-import getAst from '../getAst';
-describe('getAst', function () {
-  test('return AST', function () {
-    var result = getAst("42");
-    expect(result).toMatchSnapshot();
-  });
-});
